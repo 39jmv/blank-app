b95873f162022f822b20913540052f9bfb339ef8
39jmv <39jmv97@gmail.com>
2024-12-20 05:24:57 +0000

    primer cambio en git cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.title("PRIMER TITULO CREADO")
+st.write("Primer texto creado en Streamlit, me gusta [Link 22,200 monedas] (https://www.youtube.com/watch?v=LdystjGNEaY)")
+
+Cantidad=st.slider("Selecciona la cantidad")
+
+st.write(f"la cantidad seleccionada es {Cantidad}")
+
+st.button("Primer Boton")
+
+for i in range(0, Cantidad):
+    st.button(f"{i}")
